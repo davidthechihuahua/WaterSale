3f6a79ac54d7b86a0174a3c949b9b8bf7d91a414
Student <student@Admins-MacBook-Air.local>
2018-12-08 15:39:51 -0800

    Initial commit

---
+++ app/src/main/java/com/teamedge/android/watersale/MainActivity.java
@@ -0,0 +1,14 @@
+package com.teamedge.android.watersale;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
